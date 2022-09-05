c15ca5eb0b9e6198fe960272fac872bd9143050e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-05 10:10:20 +0300

    add handleGET()

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -19,68 +19,64 @@ import prisma from '../../../lib/prisma';
 //     process.exit(1);
 //   });
 
-// /api/post
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    // Get list
-    // /api/posts?_end=10&_order=ASC&_sort=id&_start=0
-    if (req.query._sort) {
-      const { _start, _end, _sort, _order } = req.query;
-      let allData;
-      let count;
+function getOprions(queryParams) {
+  const options = {};
+  const { _start, _end, _sort, _order, id, login } = queryParams;
 
-      console.log(req.query);
-      // console.log(range);
-      try {
-        allData = await prisma.post.findMany({
-          skip: +_start,
-          take: _end - _start,
-          orderBy: {
-            [_sort]: _order.toLowerCase(),
-          },
-        });
-        count = await prisma.post.count();
-
-        res.setHeader('X-Total-Count', count);
-        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
-        // res.setHeader('Content-Range', `cards ${_start}-${_end}/${count}`);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json(error.message);
-      }
-
-      res.status(200).json(allData);
+  if (_start && _end) {
+    options.skip = Number(_start);
+    options.take = _end - _start;
+  }
+  if (_sort && _order) {
+    options.orderBy = {
+      [_sort]: _order.toLowerCase(),
     }
-    // Get several records
-    // /api/posts?filter={"id":[123,456,789]}
-    if (req.query._filter) {
-      // const { _filter} = req.query;
-      // let allData;
-      // let count;
-
-      // console.log(req.query);
-      // // console.log(range);
-      // try {
-      //   allData = await prisma.post.findMany({
-      //     where: {
-      //       []
-      //     }
-      //   });
-      //   count = await prisma.post.count();
-
-      //   res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
-      //   res.setHeader('X-Total-Count', count);
-      //   // res.setHeader('Content-Range', `cards ${_start}-${_end}/${count}`);
-      // } catch (error) {
-      //   console.error(error);
-      //   res.status(500).json(error.message);
-      // }
-
-      // res.status(200).json(allData);
+  }
+  if (id) {
+    options.where = {
+      id: {in: id} //??
+    }
+  }else if (login) {
+    options.where = {
+      login: {
+        equals: login,
+      }
     }
   }
 
-  if (req.method === 'POST') {
+  return options;
+}
+
+async function handleGET(req, res) {
+  const options = getOprions(req.query);
+  let posts;
+  let countPosts;
+
+  try {
+    posts = await prisma.post.findMany(options);
+    countPosts = await prisma.post.count({where: options.where});
+
+    res.setHeader('X-Total-Count', countPosts);
+    res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error.message);
+  }
+}
+
+// /api/posts
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    // getlist getMany getManyReference
+    // /api/posts?_end=10&_order=ASC&_sort=id&_start=0
+    handleGET(req,res);
+   
+  }else if (req.method === 'POST') {
     // Create a record
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    )
   }
 }
