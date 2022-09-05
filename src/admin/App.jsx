41869169ceb536ffa626527ed7dddbe550a8f280
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-05 21:04:36 +0300

    rework posts resource

--- src/admin/App.jsx
+++ src/admin/App.jsx
@@ -1,15 +1,15 @@
-import * as React from "react";
+import React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import restProvider from 'ra-data-json-server';
+import { PostCreate, PostEdit, PostList, PostShow } from "./posts";
 // import jsonServerProvider from 'ra-data-json-server'; 
 
 // const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const dataProvider = restProvider('/api'); //???
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} edit={EditGuesser}/>
-    {/* <Resource name="comments" list={ListGuesser} /> */}
+  <Admin title="My Custom Admin" dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit} create={PostCreate}/>
   </Admin>
 );
 
