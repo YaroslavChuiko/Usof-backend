3f71009c8db3093bc29b754b0f758f6523591b62
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-10 22:34:03 +0300

    add comments to admin panel

--- src/admin/App.jsx
+++ src/admin/App.jsx
@@ -3,6 +3,7 @@ import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import restProvider from 'ra-data-json-server';
 import { PostCreate, PostEdit, PostList, PostShow } from './posts';
 import { UserCreate, UserEdit, UserList, UserShow } from './users';
+import { CommentCreate, CommentEdit, CommentList, CommentShow } from './comments';
 // import jsonServerProvider from 'ra-data-json-server';
 
 // const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
@@ -11,6 +12,13 @@ const dataProvider = restProvider('/api'); //???
 const App = () => (
   <Admin title="My Custom Admin" dataProvider={dataProvider}>
     <Resource
+      name="users"
+      list={UserList}
+      show={UserShow}
+      edit={UserEdit}
+      create={UserCreate}
+    />
+    <Resource
       name="posts"
       list={PostList}
       show={PostShow}
@@ -18,11 +26,11 @@ const App = () => (
       create={PostCreate}
     />
     <Resource
-      name="users"
-      list={UserList}
-      show={UserShow}
-      edit={UserEdit}
-      create={UserCreate}
+      name="comments"
+      list={CommentList}
+      show={CommentShow}
+      edit={CommentEdit}
+      create={CommentCreate}
     />
   </Admin>
 );
---
+++ src/admin/comments.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {
+  Show,
+  ShowButton,
+  SimpleShowLayout,
+  RichTextField,
+  DateField,
+  List,
+  Edit,
+  Create,
+  Datagrid,
+  ReferenceField,
+  TextField,
+  EditButton,
+  ReferenceInput,
+  SelectInput,
+  SimpleForm,
+  TextInput,
+  required,
+  EmailField,
+  NumberField,
+  PasswordInput,
+  NumberInput,
+  TabbedShowLayout,
+  Tab,
+  ReferenceManyField,
+  useRecordContext,
+  ChipField,
+  ImageInput,
+  ImageField,
+  AutocompleteInput,
+  DateInput,
+} from 'react-admin';
+
+const statuses = [
+  { status: 'active', label: 'active' },
+  { status: 'inactive', label: 'inactive' },
+];
+
+const commentFilters = [
+  <ReferenceInput label="Author" source="author_id" reference="users">
+    <AutocompleteInput optionText="login" />
+  </ReferenceInput>,
+  <ReferenceInput label="Post" source="post_id" reference="posts">
+    <AutocompleteInput optionText="title" />
+  </ReferenceInput>,
+  <SelectInput source="status" optionValue="status" optionText="label" choices={statuses} />,
+];
+
+export const CommentList = props => (
+  <List {...props} filters={commentFilters}>
+    <Datagrid rowClick="show">
+      <TextField source="id" />
+      <ReferenceField source="post_id" reference="posts" />
+      <ReferenceField label="Author" source="author_id" reference="users">
+        <TextField source="login" />
+      </ReferenceField>
+      <TextField source="status" />
+      <NumberField source="rating" />
+      <TextField source="content" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+const CommentTitle = () => {
+  const record = useRecordContext();
+  return <span>Comment: id{record ? `${record.id}` : ''}</span>;
+};
+
+export const CommentEdit = () => (
+  <Edit title={<CommentTitle />}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput label="Author" source="author_id" reference="users">
+        <SelectInput disabled optionText="login" />
+      </ReferenceInput>
+      <DateInput disabled label="Publication date" source="publish_date" />
+      <SelectInput source="status" optionValue="status" optionText="status" choices={statuses} validate={required()} />
+      <TextInput disabled multiline source="content" fullWidth />
+    </SimpleForm>
+  </Edit>
+);
+
+export const CommentCreate = () => (
+  <Create>
+    <SimpleForm>
+      <ReferenceInput label="Author" source="author_id" reference="users">
+        <AutocompleteInput optionText="login" validate={required()} />
+      </ReferenceInput>
+      <ReferenceInput label="Post" source="post_id" reference="posts">
+        <AutocompleteInput optionText="title" validate={required()} />
+      </ReferenceInput>
+      <SelectInput source="status" optionValue="status" optionText="label" choices={statuses} validate={required()} />
+      <TextInput multiline source="content" validate={required()} fullWidth />
+    </SimpleForm>
+  </Create>
+);
+
+export const CommentShow = props => (
+  <Show title={<CommentTitle />} {...props}>
+    <TabbedShowLayout spacing={2}>
+      <Tab label="Summary">
+        <SimpleShowLayout>
+          <NumberField source="id" />
+          <ReferenceField label="Post" source="post_id" reference="posts">
+            <TextField source="id" />
+          </ReferenceField>
+          <ReferenceField label="Author" source="author_id" reference="users">
+            <TextField source="login" />
+          </ReferenceField>
+          <DateField label="Publication date" source="publish_date" />
+          <TextField source="content" />
+          <NumberField source="rating" />
+          <TextField source="status" />
+        </SimpleShowLayout>
+      </Tab>
+      {/* <Tab label="Posts" path="posts">
+        <ReferenceManyField reference="posts" target="author_id" label={false}>
+          <Datagrid>
+            <TextField source="id" />
+            <ReferenceField label="Author" source="author_id" reference="users">
+              <TextField source="login" />
+            </ReferenceField>
+            <TextField source="title" />
+            <TextField source="status" />
+            <EditButton />
+            <ShowButton />
+          </Datagrid>
+        </ReferenceManyField>
+      </Tab> */}
+    </TabbedShowLayout>
+  </Show>
+);
