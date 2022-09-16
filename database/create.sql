77f6194325732dd4bb185ba74a909414174572fa
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-16 22:20:47 +0300

    add table token for email verify tokens, and add on delete cascade to foreign keys

--- database/create.sql
+++ database/create.sql
@@ -5,27 +5,37 @@ USE usof;
 -- how to save password
 -- https://stackoverflow.com/questions/247304/what-data-type-to-use-for-hashed-password-field-and-what-length 
 
+
+-- ! mb add ON DELETE CASCADE for all foreign keys becouse can't delete user with posts or something like that
 CREATE TABLE IF NOT EXISTS user (
   id INT AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(30) UNIQUE NOT NULL,
   password CHAR(60) NOT NULL,
   full_name VARCHAR(50) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
+  active BOOLEAN DEFAULT false,
   profile_picture VARCHAR(255) NOT NULL,
   rating INT DEFAULT 0 NOT NULL, -- https://metanit.com/sql/sqlserver/12.1.php#:~:text=%D0%A2%D1%80%D0%B8%D0%B3%D0%B3%D0%B5%D1%80%D1%8B%20%D0%BF%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B0%D0%B2%D0%BB%D1%8F%D1%8E%D1%82%20%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%20%D1%82%D0%B8%D0%BF%20%D1%85%D1%80%D0%B0%D0%BD%D0%B8%D0%BC%D0%BE%D0%B9,%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%20INSERT%2C%20UPDATE%2C%20DELETE.
   role ENUM('user', 'admin') DEFAULT 'user' NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS token (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+  token CHAR(128) UNIQUE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS post (
   id INT AUTO_INCREMENT PRIMARY KEY, -- id INT AUTO_INCREMENT PRIMARY KEY
   author_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user(id) ON DELETE CASCADE,
   title VARCHAR(100) NOT NULL, -- ?? size
   publish_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, -- ??DATETIME https://dev.mysql.com/doc/refman/8.0/en/timestamp-initialization.html
   content VARCHAR(5000), -- ?? type size
   status ENUM('active', 'inactive') DEFAULT 'active',
-  rating INT DEFAULT 0 NOT NULL,
+  rating INT DEFAULT 0 NOT NULL
   -- ??categories
-  FOREIGN KEY (author_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS comment (
@@ -57,11 +67,11 @@ CREATE TABLE IF NOT EXISTS post_categories (
 CREATE TABLE IF NOT EXISTS like_entity ( -- Foreign Key to multiple tables https://stackoverflow.com/questions/7844460/foreign-key-to-multiple-tables
   id INT AUTO_INCREMENT PRIMARY KEY,
   author_id INT NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES user(id),
+    FOREIGN KEY (author_id) REFERENCES user(id) ON DELETE CASCADE,
   target_post INT NULL,
-    FOREIGN KEY (target_post) REFERENCES post(id),
+    FOREIGN KEY (target_post) REFERENCES post(id) ON DELETE CASCADE,
   target_comment INT NULL,
-    FOREIGN KEY (target_comment) REFERENCES comment(id),
+    FOREIGN KEY (target_comment) REFERENCES comment(id) ON DELETE CASCADE,
   CONSTRAINT uc_like_authorPost UNIQUE(author_id, target_post),
   CONSTRAINT uc_like_authorComment UNIQUE(author_id, target_comment),
   CONSTRAINT CK_target_postComment CHECK (
