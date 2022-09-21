9b41d0515f61748896512bc5e4eb26d5760fc8cf
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-21 14:55:10 +0300

    add DEFAULT_AVATAR_PATH, UPLOADS_PATH

--- util/const.js
+++ util/const.js
@@ -1,5 +1,9 @@
+import path from 'path';
+
 export const TYPE_SUCCESS = 'success';
 export const TYPE_ERROR = 'error';
 export const SAULT_ROUNDS = 10;
 export const TOKEN_EXPIRE_SEC = 60 * 60 * 2; // 2h
 export const TOKEN_SECRET = 'secret';
+export const DEFAULT_AVATAR_PATH = 'defaultAvatar/defaultAvatar.jpg';
+export const UPLOADS_PATH = path.join(process.cwd(), 'public', 'uploads');
