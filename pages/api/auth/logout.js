fb737bb7c17c5ed6aef842977ab98318d0690a3c
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-21 14:58:59 +0300

    refactor

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -3,16 +3,16 @@ import Cookies from 'cookies';
 // /api/auth/logout
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    logout(res, req);
+    logout(req, res);
   } else {
     res.status(405).end(`The HTTP ${req.method} method is not supported at this route.`);
   }
 }
 
-async function logout(res, req) {
+async function logout(req, res) {
   const cookies = new Cookies(req, res);
   cookies.set('token'); // Delete a cookie
 
-  res.status(200).json({message: 'Success logout'});
+  res.status(200).json({success: true, message: 'Success logout'});
 }
 
