4db153520f457e96b47224b01e7bb725ffed6afa
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-22 18:11:39 +0300

    refactor: del comments

--- logic/SimpleCRUD.js
+++ logic/SimpleCRUD.js
@@ -1,7 +1,6 @@
 class SimpleCRUD {
   constructor() {}
 
-  // GET http://my.api.url/posts?_sort=title&_order=ASC&_start=0&_end=24&title=bar
   static async getList(options, prismaModelEntity) {
     const entities = await prismaModelEntity.findMany(options);
     const countEntities = await prismaModelEntity.count({ where: options.where });
@@ -9,7 +8,6 @@ class SimpleCRUD {
     return [entities, countEntities];
   }
 
-  // GET http://my.api.url/posts/123
   static async getOne(id, prismaModelEntity) {
     const entity = await prismaModelEntity.findUnique({
       where: {
@@ -20,7 +18,6 @@ class SimpleCRUD {
     return entity;
   }
 
-  // POST http://my.api.url/posts
   static async create(data, prismaModelEntity) {
     const entity = await prismaModelEntity.create({
       data: { ...data },
@@ -29,7 +26,6 @@ class SimpleCRUD {
     return entity;
   }
 
-  // PUT http://my.api.url/posts/123
   static async update(id, data, prismaModelEntity) {
     const entity = await prismaModelEntity.update({
       where: { id: Number(id) },
@@ -39,7 +35,6 @@ class SimpleCRUD {
     return entity;
   }
 
-  // DELETE http://my.api.url/posts/123
   static async delete(id, prismaModelEntity) {
     const entity = await prismaModelEntity.delete({
       where: { id: Number(id) },
