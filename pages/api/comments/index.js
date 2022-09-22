2d67089efd87b3f17de023528d84f0f77b30713a
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-22 19:08:35 +0300

    change permissions

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,17 +1,13 @@
 import prisma from '../../../lib/prisma';
 import SimpleCRUD from '../../../logic/SimpleCRUD';
-import { withAuthAdmin } from '../../../util/auth';
+import { withAuthUser } from '../../../util/auth';
 
 // /api/comments
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const result = withAuthAdmin(req, res);
-    if (!result.success) return;
-    req.user = result.decoded;
-
     handleGET(req, res);
   } else if (req.method === 'POST') {
-    const result = withAuthAdmin(req, res);
+    const result = withAuthUser(req, res);
     if (!result.success) return;
     req.user = result.decoded;
 
