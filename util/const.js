915cf40e6f1535d254e0998acc623521909368c5
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-22 19:07:34 +0300

    delete TYPE_SUCCESS, TYPE_ERROR

--- util/const.js
+++ util/const.js
@@ -1,7 +1,5 @@
 import path from 'path';
 
-export const TYPE_SUCCESS = 'success';
-export const TYPE_ERROR = 'error';
 export const SAULT_ROUNDS = 10;
 export const TOKEN_EXPIRE_SEC = 60 * 60 * 2; // 2h
 export const TOKEN_SECRET = 'secret';
