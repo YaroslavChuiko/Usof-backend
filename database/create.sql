33b0cd75f1040b78e174524416214a937a873547
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-22 18:10:13 +0300

    update triggers and delete comments

--- database/create.sql
+++ database/create.sql
@@ -6,8 +6,6 @@ USE usof;
 -- how to save password
 -- https://stackoverflow.com/questions/247304/what-data-type-to-use-for-hashed-password-field-and-what-length 
 
-
--- ! mb add ON DELETE CASCADE for all foreign keys becouse can't delete user with posts or something like that
 CREATE TABLE IF NOT EXISTS user (
   id INT AUTO_INCREMENT PRIMARY KEY,
   login VARCHAR(30) UNIQUE NOT NULL,
@@ -16,7 +14,7 @@ CREATE TABLE IF NOT EXISTS user (
   email VARCHAR(255) UNIQUE NOT NULL,
   active BOOLEAN DEFAULT false,
   profile_picture VARCHAR(255) NOT NULL,
-  rating INT DEFAULT 0 NOT NULL, -- https://metanit.com/sql/sqlserver/12.1.php#:~:text=%D0%A2%D1%80%D0%B8%D0%B3%D0%B3%D0%B5%D1%80%D1%8B%20%D0%BF%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B0%D0%B2%D0%BB%D1%8F%D1%8E%D1%82%20%D1%81%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%20%D1%82%D0%B8%D0%BF%20%D1%85%D1%80%D0%B0%D0%BD%D0%B8%D0%BC%D0%BE%D0%B9,%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%20INSERT%2C%20UPDATE%2C%20DELETE.
+  rating INT DEFAULT 0 NOT NULL,
   role ENUM('user', 'admin') DEFAULT 'user' NOT NULL
 );
 
@@ -42,11 +40,12 @@ CREATE TABLE IF NOT EXISTS email_token (
 );
 
 CREATE TABLE IF NOT EXISTS post (
-  id INT AUTO_INCREMENT PRIMARY KEY, -- id INT AUTO_INCREMENT PRIMARY KEY
+  id INT AUTO_INCREMENT PRIMARY KEY,
   author_id INT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES user(id) ON DELETE CASCADE,
   title VARCHAR(100) NOT NULL, -- ?? size
-  publish_date DATETIME DEFAULT NOW() NOT NULL, -- ??DATETIME https://dev.mysql.com/doc/refman/8.0/en/timestamp-initialization.html
+  -- publish_date DATETIME DEFAULT NOW() NOT NULL, -- ??DATETIME https://dev.mysql.com/doc/refman/8.0/en/timestamp-initialization.html
+  publish_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   content VARCHAR(5000), -- ?? type size
   status ENUM('active', 'inactive') DEFAULT 'active',
   rating INT DEFAULT 0 NOT NULL
@@ -68,7 +67,7 @@ CREATE TABLE IF NOT EXISTS comment (
 CREATE TABLE IF NOT EXISTS category (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
-  description VARCHAR(500) NOT NULL -- ?? size
+  description VARCHAR(500) DEFAULT '' NOT NULL -- ?? size
 );
 
 CREATE TABLE IF NOT EXISTS post_categories (
@@ -79,7 +78,7 @@ CREATE TABLE IF NOT EXISTS post_categories (
   FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS like_entity ( -- Foreign Key to multiple tables https://stackoverflow.com/questions/7844460/foreign-key-to-multiple-tables
+CREATE TABLE IF NOT EXISTS like_entity (
   id INT AUTO_INCREMENT PRIMARY KEY,
   author_id INT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES user(id) ON DELETE CASCADE,
@@ -102,25 +101,35 @@ CREATE TRIGGER IF NOT EXISTS after_like_insert
 AFTER INSERT
 ON like_entity FOR EACH ROW
 BEGIN
-  DECLARE newRating, authorId INT;
-
-  IF NEW.target_post IS NOT NULL THEN
-    SET authorId = (SELECT author_id FROM post WHERE id = NEW.target_post);
-  ELSE
-    SET authorId = (SELECT author_id FROM comment WHERE id = NEW.target_comment);
-  END IF;
-  
-  SET newRating = (SELECT rating FROM user WHERE id = authorId);
+  DECLARE userRating, postRating, commentRating, ratingChange, authorId INT;
 
   CASE NEW.type
     WHEN 'like' THEN
-      SET newRating = newRating + 1;
+      SET ratingChange = 1;
     WHEN 'dislike' THEN
-      SET newRating = newRating - 1;
+      SET ratingChange = -1;
   END CASE;
 
+  IF NEW.target_post IS NOT NULL THEN
+    SET authorId = (SELECT author_id FROM post WHERE id = NEW.target_post);
+    SET postRating = (SELECT rating FROM post WHERE id = NEW.target_post); 
+    UPDATE post
+    SET rating = postRating + ratingChange
+    WHERE
+      id = NEW.target_post;
+  ELSE
+    SET authorId = (SELECT author_id FROM comment WHERE id = NEW.target_comment);
+    SET commentRating = (SELECT rating FROM comment WHERE id = NEW.target_comment);
+    UPDATE comment
+    SET rating = commentRating + ratingChange
+    WHERE
+      id = NEW.target_comment;
+  END IF;
+  
+  SET userRating = (SELECT rating FROM user WHERE id = authorId);
+
   UPDATE user
-  SET rating = newRating
+  SET rating = userRating + ratingChange
   WHERE
     id = authorId;
   
@@ -133,28 +142,38 @@ CREATE TRIGGER IF NOT EXISTS after_like_update
 AFTER UPDATE
 ON like_entity FOR EACH ROW
 BEGIN
-  DECLARE newRating, authorId INT;
+  DECLARE userRating, postRating, commentRating, ratingChange, authorId INT;
+  IF OLD.type != NEW.type THEN
+    CASE NEW.type
+      WHEN 'like' THEN
+        SET ratingChange = 2;
+      WHEN 'dislike' THEN
+        SET ratingChange = -2;
+    END CASE;
 
-  IF NEW.target_post IS NOT NULL THEN
-    SET authorId = (SELECT author_id FROM post WHERE id = NEW.target_post);
-  ELSE
-    SET authorId = (SELECT author_id FROM comment WHERE id = NEW.target_comment);
+    IF NEW.target_post IS NOT NULL THEN
+      SET authorId = (SELECT author_id FROM post WHERE id = NEW.target_post);
+      SET postRating = (SELECT rating FROM post WHERE id = NEW.target_post); 
+      UPDATE post
+      SET rating = postRating + ratingChange
+      WHERE
+        id = NEW.target_post;
+    ELSE
+      SET authorId = (SELECT author_id FROM comment WHERE id = NEW.target_comment);
+      SET commentRating = (SELECT rating FROM comment WHERE id = NEW.target_comment);
+      UPDATE comment
+      SET rating = commentRating + ratingChange
+      WHERE
+        id = NEW.target_comment;
+    END IF;
+    
+    SET userRating = (SELECT rating FROM user WHERE id = authorId);
+    
+    UPDATE user
+    SET rating = userRating + ratingChange
+    WHERE
+      id = authorId;
   END IF;
-
-  SET newRating = (SELECT rating FROM user WHERE id = authorId);
-
-  CASE NEW.type
-    WHEN 'like' THEN
-      SET newRating = newRating + 2;
-    WHEN 'dislike' THEN
-      SET newRating = newRating - 2;
-  END CASE;
-
-  UPDATE user
-  SET rating = newRating
-  WHERE
-    id = authorId;
-  
 END
 //
 DELIMITER ;
@@ -164,27 +183,38 @@ CREATE TRIGGER IF NOT EXISTS after_like_delete
 AFTER DELETE
 ON like_entity FOR EACH ROW
 BEGIN
-  DECLARE newRating, authorId INT;
+  DECLARE userRating, postRating, commentRating, ratingChange, authorId INT;
+  
+  CASE OLD.type
+    WHEN 'like' THEN
+      SET ratingChange = -1;
+    WHEN 'dislike' THEN
+      SET ratingChange = 1;
+  END CASE;
 
   IF OLD.target_post IS NOT NULL THEN
     SET authorId = (SELECT author_id FROM post WHERE id = OLD.target_post);
+    SET postRating = (SELECT rating FROM post WHERE id = OLD.target_post); 
+    UPDATE post
+    SET rating = postRating + ratingChange
+    WHERE
+      id = OLD.target_post;
   ELSE
     SET authorId = (SELECT author_id FROM comment WHERE id = OLD.target_comment);
+    SET commentRating = (SELECT rating FROM comment WHERE id = OLD.target_comment);
+    UPDATE comment
+    SET rating = commentRating + ratingChange
+    WHERE
+      id = OLD.target_comment;
   END IF;
-  SET newRating = (SELECT rating FROM user WHERE id = authorId);
-
-  CASE OLD.type
-    WHEN 'like' THEN
-      SET newRating = newRating - 1;
-    WHEN 'dislike' THEN
-      SET newRating = newRating + 1;
-  END CASE;
-
+  
+  SET userRating = (SELECT rating FROM user WHERE id = authorId);
+  
   UPDATE user
-  SET rating = newRating
+  SET rating = userRating + ratingChange
   WHERE
     id = authorId;
-  
+
 END
 //
 DELIMITER ;
