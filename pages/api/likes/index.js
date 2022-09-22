3b2fe7579e2742b651655cd305b7b465716c2aba
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-22 19:09:15 +0300

    change permissions, add handleDELETE

--- pages/api/likes/index.js
+++ pages/api/likes/index.js
@@ -1,17 +1,23 @@
 import prisma from '../../../lib/prisma';
 import SimpleCRUD from '../../../logic/SimpleCRUD';
-import { withAuthAdmin } from '../../../util/auth';
+import { withAuthUser } from '../../../util/auth';
 
 // /api/likes
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const result = withAuthAdmin(req, res);
+    handleGET(req, res);
+  } else if (req.method === 'POST') {
+    const result = withAuthUser(req, res);
     if (!result.success) return;
     req.user = result.decoded;
 
-    handleGET(req, res);
-  } else if (req.method === 'POST') {
-    handlePOST(req.body, res);
+    handlePOST(req, res);
+  } else if (req.method === 'DELETE') {
+    const result = withAuthUser(req, res);
+    if (!result.success) return;
+    req.user = result.decoded;
+
+    handleDELETE(req, res);
   } else {
     res.status(405).end(`The HTTP ${req.method} method is not supported at this route.`);
   }
@@ -89,23 +95,105 @@ async function handleGET(req, res) {
   }
 }
 
-// POST /api/likes/[likeId]
-// async function handlePOST(data, res) {
-//   const { author_id, post_id, content, status } = data;
-//   // console.log(data);
+// POST /api/likes
+async function handlePOST(req, res) {
+  const { author_id, target_post, target_comment, type } = req.body;
 
-//   try {
-//     const newlike = await prisma.like_entity.create({
-//       data: {
-//         author_id: author_id,
-//         post_id: post_id,
-//         content: content,
-//         status: status,
-//       },
-//     });
-//     res.status(200).json(newlike);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Database error' });
-//   }
-// }
+  if (req.user.id != author_id) {
+    return res.status(403).json({ message: "You don't have enough access rights" });
+  }
+
+  const newLikeData = {
+    author_id: author_id,
+    target_post: target_post || null,
+    target_comment: target_comment || null,
+    type: type,
+  };
+
+  try {
+    if (target_post && target_comment) {
+      throw new Error('Only one of target_post or target_comment can be set');
+    }
+
+    if (author_id && target_post) {
+      const newLike = await prisma.like_entity.upsert({
+        where: {
+          author_id_target_post: {
+            author_id: author_id,
+            target_post: target_post,
+          },
+        },
+        create: { ...newLikeData },
+        update: { type: type },
+      });
+
+      return res.status(200).json(newLike);
+    }
+
+    if (author_id && target_comment) {
+      const newLike = await prisma.like_entity.upsert({
+        where: {
+          author_id_target_comment: {
+            author_id: author_id,
+            target_comment: target_comment,
+          },
+        },
+        create: { ...newLikeData },
+        update: { type: type },
+      });
+
+      return res.status(200).json(newLike);
+    }
+
+    throw new Error();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Database error' });
+  }
+}
+
+// DELETE /api/likes
+async function handleDELETE(req, res) {
+  const { author_id, target_post, target_comment } = req.body;
+
+  if (req.user.id != author_id) {
+    return res.status(403).json({ message: "You don't have enough access rights" });
+  }
+
+  try {
+    if (target_post && target_comment) {
+      throw new Error('Only one of target_post or target_comment can be set');
+    }
+
+    if (author_id && target_post) {
+      const deletedLike = await prisma.like_entity.delete({
+        where: {
+          author_id_target_post: {
+            author_id: Number(author_id),
+            target_post: Number(target_post),
+          },
+        },
+      });
+
+      return res.status(200).json(deletedLike);
+    }
+
+    if (author_id && target_comment) {
+      const deletedLike = await prisma.like_entity.delete({
+        where: {
+          author_id_target_comment: {
+            author_id: Number(author_id),
+            target_comment: Number(target_comment),
+          },
+        },
+      });
+
+      return res.status(200).json(deletedLike);
+    }
+
+    throw new Error();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Database error' });
+  }
+}
