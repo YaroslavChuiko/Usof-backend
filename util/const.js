3db4f91049f5bceb413421cc9184cd11887da642
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-30 20:39:22 +0300

    update TOKEN_EXPIRE_SEC

--- util/const.js
+++ util/const.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export const SAULT_ROUNDS = 10;
-export const TOKEN_EXPIRE_SEC = 60 * 60 * 2; // 2h
+export const TOKEN_EXPIRE_SEC = 60 * 60 * 24 * 3; // 3 days
 export const TOKEN_SECRET = 'secret';
 export const DEFAULT_AVATAR_PATH = 'defaultAvatar/defaultAvatar.jpg';
 export const UPLOADS_PATH = path.join(process.cwd(), 'public', 'uploads');
