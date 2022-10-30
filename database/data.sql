06a1cdd8c43a1c6dd4793893e10584008bf2f1ec
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-30 12:09:08 +0200

    update

--- database/data.sql
+++ database/data.sql
@@ -46,7 +46,9 @@ INSERT INTO comment (author_id, answer_id, content)
 VALUES
   (1, 1, 'laudantium enim quasi est quidem magnam voluptate ipsam eos tempora quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente accusantium'),
   (2, 1, 'est natus enim nihil est dolore omnis voluptatem numquam et omnis occaecati quod ullam at voluptatem error expedita pariatur nihil sint nostrum voluptatem reiciendis et'),
-  (3, 1, 'quia molestiae reprehenderit quasi aspernatur aut expedita occaecati aliquam eveniet laudantium omnis quibusdam delectus saepe quia accusamus maiores nam est cum et ducimus et vero voluptates excepturi deleniti ratione');
+  (3, 1, 'quia molestiae reprehenderit quasi aspernatur aut expedita occaecati aliquam eveniet laudantium omnis quibusdam delectus saepe quia accusamus maiores nam est cum et ducimus et vero voluptates excepturi deleniti ratione'),
+  (3, 2, 'quia molestiae reprehenderit quasi aspernatur aut expedita occaecati aliquam eveniet laudantium omnis quibusdam delectus saepe quia accusamus maiores nam est cum et ducimus et vero voluptates excepturi deleniti ratione'),
+  (1, 2, 'quia molestiae reprehenderit quasi aspernatur aut expedita occaecati aliquam eveniet laudantium omnis quibusdam delectus saepe quia accusamus maiores nam est cum et ducimus et vero voluptates excepturi deleniti ratione');
 
 INSERT INTO category (title, description)
 VALUES
